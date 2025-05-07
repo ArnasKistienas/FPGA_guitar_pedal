5c1886ff0a305dd5a5d376ea36ec999cf27971ab
Cobbalt7 <>
2025-05-07 21:51:32 +0300

    Added I2S receiver module

---
+++ I2S/src/i2s_receiver.sv
@@ -0,0 +1,57 @@
+module i2s_transmitter(
+        input rst,
+        input mclk,
+        input sclk,
+        input lrclk,
+        input sdin,
+        output reg [23:0]data,
+        output reg dvalid,
+        output mclk_out,
+        output sclk_out,
+        output lrclk_out
+    );
+    assign mclk_out = mclk;
+    assign sclk_out = sclk;
+    assign lrclk_out = lrclk;
+
+    // Iterators
+    reg [4:0] counter;
+    reg prev_lr;
+
+    // State encodings
+    parameter
+        IDLE        = 1'b0,
+        READ        = 1'b1;
+    reg state;
+  
+    // State transitions
+    always @(posedge sclk) begin
+        if(rst) begin
+            prev_lr = lrclk;
+            dvalid = 1'b0;
+            state = IDLE;
+        end
+        case (state)
+        IDLE:
+            begin
+                if(prev_lr ^ lrclk) begin
+                    counter = 5'd23;
+                    dvalid <= 1'b0;
+                    state <= READ;
+                end
+                prev_lr <= lrclk;
+            end
+        READ:
+            begin          
+                data[counter] <= sdin;
+                if(counter == 5'd0) begin
+                    dvalid <= 1'b1;
+                    state <= IDLE;
+                end
+                counter--;
+            end
+        default:
+            state <= IDLE;
+        endcase
+    end
+endmodule
---
+++ I2S/src/tb_i2s_receiver.sv
@@ -0,0 +1,61 @@
+`timescale 10ps / 10ps
+
+module tb_i2s_receiver;
+    reg reset, in_mclk;
+    wire [23:0] out_data;
+    wire in_sclk, in_lrclk, out_mclk, out_sclk, out_lrclk, data_valid;
+    reg in;
+    integer count, I;
+    i2s_clock_divider TEST_HELP(
+        .rst(reset),
+        .mclk(in_mclk),
+        .sclk(in_sclk),
+        .lrclk(in_lrclk)
+    );
+    i2s_transmitter DUT(
+        .rst(reset),
+        .mclk(in_mclk),
+        .sclk(in_sclk),
+        .lrclk(in_lrclk),
+        .sdin(in),
+        .data(out_data),
+        .dvalid(data_valid),
+        .mclk_out(out_mclk),
+        .sclk_out(out_sclk),
+        .lrclk_out(out_lrclk)
+    );
+    initial begin
+        $dumpfile("../sim/i2s_receiver.vcd");
+        $dumpvars(1, tb_i2s_receiver);
+        forever #2214 in_mclk= ~in_mclk;
+    end
+    initial begin
+        reset = 1'b1;
+        count = 1'b0;
+        in_mclk=1'b1;
+        #2215 reset = 0;
+        send_chunk(24'd50321);
+        #(4428*8*7);
+        send_chunk(24'd16777215);
+        #(4428*8*7);
+        send_chunk(24'd0);
+        #(4428*8*7);
+        send_chunk(24'd34245);
+        #(4428*8*7);
+        $finish(1);
+    end
+
+    always @(in_lrclk) begin
+
+    end
+    task send_chunk(input [23:0] message);
+        begin
+            #(4428*8);
+            for(I = 23; I >= 0; I--)
+                begin
+                    in = message[I];
+                    #(4428*8);
+                end
+        end
+    endtask
+endmodule
